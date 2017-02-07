9f056dcc889ce611081ce38a9a79d4385a106b0d
amisolution <info@amisolution.net>
2017-02-07 11:52:39 +0100

    Update Mobile.sol

--- Mobile.sol
+++ Mobile.sol
@@ -11,21 +11,6 @@ Anyone is free to copy, modify, publish, use, compile, sell, or
 distribute this software, either in source code form or as a compiled
 binary, for any purpose, commercial or non-commercial, and by any
 means.
-In jurisdictions that recognize copyright laws, the author or authors
-of this software dedicate any and all copyright interest in the
-software to the public domain. We make this dedication for the benefit
-of the public at large and to the detriment of our heirs and
-successors. We intend this dedication to be an overt act of
-relinquishment in perpetuity of all present and future rights to this
-software under copyright law.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-OTHER DEALINGS IN THE SOFTWARE.
-For more information, please refer to <http://unlicense.org>
 Challenges of setting contract:
 => Additional TimeFrame of Delivery required
 => Short Contract duration TimeFrame cannot be handled by large mobile carrier.
@@ -56,27 +41,8 @@ contract ContractDestruction{
 
 		suicide(owner);
 	}
-    
-    	function ownerBalanceChecker() public 
-    	{
-        owner = msg.sender; 								 // msg is a global variable
-        ownerbalance = owner.balance;
-    	}
 
-	function getContractAddress() constant returns (address) 
-	{
-		return this;
-	}
 
-	function getOwnerBalanceOld() constant returns (uint)     // Will return the owner's balance AT THE TIME THIS CONTRACT WAS CREATED
-	{
-        return ownerbalance;
-    	}
-    
-    	function getOwnerBalanceNow() constant returns (uint)  // Will return owner's balance NOW
-    	{
-        return owner.balance;
-    	}
 }
 
 contract MyUserName is ContractDestruction{
@@ -137,19 +103,16 @@ contract ServiceProvider is ContractDestruction{
 
 	string public ServiceProvider;
 	string public operator;
-	string public cellbill;
-	string public utilitybill;
+	string public servicebill;
 	
 	function ServiceProvider(
 		string _UserId,
 		string _operator,
-		string _PayMyCellBill,
-		string _PayMyUtilityBill){
+		string _PayMyBill){
 
 		ServiceProvider = _UserId;
 		operator  = _operator;
-		cellbill  = _PayMyCellBill;
-		utilitybill  = _PayMyUtilityBill;
+		servicebill  = _PayMyBill;
 
 	}
 
@@ -161,4 +124,3 @@ contract ServiceProvider is ContractDestruction{
 	}
 
 }
-
